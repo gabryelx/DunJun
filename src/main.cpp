f3f5eb8cc7df31da76080f8335613f230e00cc56
gabryelx <fuzzybunny3d@gmail.com>
2015-03-03 09:43:42 -0800

    Day 003 - Drawing with OpenGL (GLEW)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <Dunjun/Common.hpp>
+#include <gl/glew.h>
 #include <GLFW/glfw3.h>
 
 GLOBAL const int g_windowWidth = 854;
 GLOBAL const int g_windowHeight = 480;
+
+void glfwHints()
+{
+	glfwWindowHint(GLFW_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_VERSION_MINOR, 1);
+}
 
 int main(int argc, char** argv)
 {
@@ -26,6 +33,58 @@ int main(int argc, char** argv)
 	//make the window's context current
 	glfwMakeContextCurrent(window);
 
+	glewInit();
+
+	float vertices[] = {
+		0.0f, 0.5f,  // vertex 1
+		-0.5f, -0.5f, // vertex 2
+		0.5f, -0.5f  //vertex 3
+	};
+
+	GLuint vbo;  //vertex buffer object
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const char * vertexShaderText = {
+		"#version 120\n"
+		"\n"
+		"attribute vec2 position;"
+		"void main()"
+		"{"
+		"   gl_Position = vec4(position, 0.0, 1.0);"
+		"}"
+	};
+
+	const char * fragmentShaderText{
+		"#version 120\n"
+		"\n"
+		"void main()"
+		"{"
+		"   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
+		"}"
+	};
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderText, nullptr);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderText, nullptr);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader); 
+
+	glBindAttribLocation(shaderProgram, 0, "position");
+
+	glLinkProgram(shaderProgram);
+
+	glUseProgram(shaderProgram);
+
+
+
 	//loop until the use closes the window
 	bool running = true;
 	bool fullscreen = false;
@@ -33,6 +92,17 @@ int main(int argc, char** argv)
 	{
 		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//draw things
+		{
+			glEnableVertexAttribArray(0);
+
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
+			glDisableVertexAttribArray(0);
+		}
 
 		//swap front and back buffers
 		glfwSwapBuffers(window);
